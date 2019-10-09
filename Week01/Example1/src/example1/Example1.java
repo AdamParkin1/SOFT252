7ffffee0c6e26ef7b9f8d0b89921d515aed53c02
Adam <44804633+AdamParkin1@users.noreply.github.com>
2019-10-09 11:24:30 +0100

    Add files via upload
    first

---
+++ Week01/Example1/src/example1/Example1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example1;
+
+/**
+ *
+ * @author tallenbrook
+ */
+public class Example1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+    // TODO code application logic here
+    int day = 22;
+    System.out.println("Hello world!");
+    System.out.println("The date today is " + day);
+    System.out.println(true);
+    System.exit(0); 
+ }
+    
+}
